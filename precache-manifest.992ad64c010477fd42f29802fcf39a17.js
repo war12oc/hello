f7431ebfdb44dc7931868d85624d577d97586d6c
war12oc <ganglovesee@gmail.com>
2019-05-07 20:00:42 +0700

    Updates

---
+++ precache-manifest.992ad64c010477fd42f29802fcf39a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470409ed6f5b4142fc3e0fdc6c2a7be3",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "3110cbfd6d71ad9f9c2a",
+    "url": "/hello/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/hello/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3110cbfd6d71ad9f9c2a",
+    "url": "/hello/static/js/main.f6820e5d.chunk.js"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  }
+]);
